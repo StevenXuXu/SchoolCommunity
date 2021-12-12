9a55c9d0d092d8c7d5489f7840215e471503f71e
StevenXuXu <xsjgreat@163.com>
2021-12-12 01:04:39 -0800

    导入相关包

--- demo.js
+++ demo.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const multer  = require('multer')
+const path  = require('path')
 const port = 3001
 
 app.set('view engine', 'ejs')//将ejs集成到express中
@@ -10,14 +14,52 @@ app.use(express.static('static'))
 app.use(express.static('node_modules'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cookieParser('jiami'))
+app.use(session({
+    secret: 'this is a session', //服务器生成session签名
+    name: 'username',
+    resave: false, //强制保存session即使他没有变化
+    saveUninitialized: true, //强制保存未初始化的session
+    cookie: {
+        maxAge: 1000 * 60 * 15
+    },
+    rolling: true
+}))
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'static/upload')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      var extname = path.extname(file.originalname)
+      cb(null, file.fieldname + '-' + uniqueSuffix + '.' + extname)
+    }
+  })
 
 app.get('/', (req, res) => {
-    
     res.render('login.ejs')
 })
+
 app.get('/login.ejs', (req, res) => {
-    
     res.render('login.ejs')
+})
+
+app.post('/doLogin', (req, res) => {
+    var username = req.body.inputusername
+    var password = req.body.inputpassword
+    req.session.username = username
+    res.send(username + ' ' + password)
+})
+
+app.get('/logOut', (req, res) => {
+    req.session.username = ""
+    res.send('logout')
+})
+
+app.use('/haha', (req, res) => {
+    var username = req.session.username
+    if(!username) res.send('nonono')
+    else res.send(username)
 })
 
 app.get('/reg.ejs', (req, res) => {
@@ -25,7 +67,7 @@ app.get('/reg.ejs', (req, res) => {
 })
 
 app.use((req, res) => {
-    res.status(404).send("404 NOT FOUND");
+    res.status(404).send(req.url + 'not found');
 })
 
 app.listen(port, () => {
