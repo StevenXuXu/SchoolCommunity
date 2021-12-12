b10df37b1e8f0c0daecdb2c1b2213bb7344ad329
StevenXuXu <xsjgreat@163.com>
2021-12-11 21:30:17 -0800

    实现reg界面

--- demo.js
+++ demo.js
@@ -1,30 +1,31 @@
-const exp = require('constants')
 const express = require('express')
 const app = express()
 const fs = require('fs')
-
+const bodyParser = require('body-parser')
 const port = 3001
 
-app.use('/', express.static('node_modules'))
+app.set('view engine', 'ejs')//将ejs集成到express中
 
-app.get('/images*', (req, res) => {
-    var name = req.url.split('/')[2]
-    fs.readFile('images/' + name, function (err, data) {
-        if (err) return console.error(err)
-        //console.log(data.toString())
-        res.write(data)
-        res.end()
-    })
-})
+app.use(express.static('static'))
+app.use(express.static('node_modules'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     
-    fs.readFile('login.html', function (err, data) {
-        if (err) return console.error(err)
-        //console.log(data.toString())
-        res.write(data)
-        res.end()
-    })
+    res.render('login.ejs')
+})
+app.get('/login.ejs', (req, res) => {
+    
+    res.render('login.ejs')
+})
+
+app.get('/reg.ejs', (req, res) => {
+    res.render('reg.ejs')
+})
+
+app.use((req, res) => {
+    res.status(404).send("404 NOT FOUND");
 })
 
 app.listen(port, () => {
